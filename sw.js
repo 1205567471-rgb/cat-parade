521cd827cf38d30fdd99578f134080bef05224ed
wjj <wjj@local>
2026-08-24 23:32:47 +0800

    service worker cache

---
+++ sw.js
@@ -0,0 +1,15 @@
+const C = 'cat-parade-v2';
+const ASSETS = ['./','./index.html','./index.js','./index.wasm','./index.pck','./index.audio.worklet.js','./index.audio.position.worklet.js','./index.icon.png'];
+self.addEventListener('install', e => { e.waitUntil(caches.open(C).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())); });
+self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== C).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
+self.addEventListener('fetch', e => {
+  const url = new URL(e.request.url);
+  if (e.request.method !== 'GET' || url.origin !== location.origin) return;
+  e.respondWith(
+    caches.match(e.request, { ignoreSearch: true }).then(hit => hit || fetch(e.request).then(res => {
+      const copy = res.clone();
+      caches.open(C).then(c => c.put(e.request, copy));
+      return res;
+    }).catch(() => hit))
+  );
+});
